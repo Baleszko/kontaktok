5c84f3a47ce14138db623eb2e306814badaf47cc
Bálint Tóth <tothbalesz@gmail.com>
2023-06-30 11:52:24 +0200

    db and entity

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 A Symfonyt composerrel telepítettem, majd behúztam a webapp packaget hozzá.
 Az annotations csomagot telepítettem a routinghoz, a MakerBundlet a Controllerek és Entityk generálásához,
-az Encoret a Webpack telepítéséhez, amivel behúztam a Bootstrapet.
+Az Encoret a Webpack telepítéséhez, amivel behúztam a Bootstrapet.
 A Doctrin orm használatához a symfony/orm-packet telepítettem.
+A formhoz a symfony/form csomagot.
 
 Első alkalom:
 Composer install
 npm install
 
-Adatbázis generálási beállítások:
-db_név: contacts
-DATABASE_URL="mysql://"felhasználónév":"jelszó"@127.0.0.1:3306/"db_név"?serverVersion=10.11.2-MariaDB&charset=utf8mb4"
+Az egyszerűbb beállítások miatt a .env fájlt is feltöltöttem githubra.
 
-webpack.config.js Scss engedéjezése, .enableSassLoader(); kikommentezés
-// enables Sass/SCSS support
-.enableSassLoader();
+A következő parancsokkal létre lehet hozni az adatbázist(Én xamppot telepítettem, szóval MariaDB-t használtam), majd le kell futtatni a migrációs fájlokat:
+
+php bin/console doctrine:database:create
+php bin/console doctrine:migrations:migrate
 
 Dev server:
 symfony server:start
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form-> handleRequest($request);
+
+        if($form->isSubmitted()){
+            if(!empty( $_POST['contact']['name']) &&  !empty( $_POST['contact']['email']) &&  !empty( $_POST['contact']['text'])){              
+                
+
+                $contact->setName( $_POST['contact']['name']);
+                $contact->setEmail( $_POST['contact']['email']);
+                $contact->setText( $_POST['contact']['text']);
+
+                $entityManager->persist($contact);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Köszönjük szépen a kérdésedet.
+                Válaszunkkal hamarosan keresünk a megadott e-mail címen.');
+            }else{
+
+                
+                $this->addFlash('fail', "Hiba! Kérjük töltsd ki az
+                összes mezőt!"
+            );
+        }
+            
+        }
+
+        // $this->addFlash('success', 'Köszönjük szépen a kérdésedet.
+        // Válaszunkkal hamarosan keresünk a megadott e-mail címen.');
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form,
         ]);
     }
 }
